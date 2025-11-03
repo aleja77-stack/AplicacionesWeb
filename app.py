f754373bb04609ecb8f3b1e62cd148e0f8061b9e
aleja77-stack <erikaleja.77@gmail.com>
2025-11-03 15:54:36 -0500

    Fix typos in Spanish text for app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
 import streamlit as st
 
 st.header('Lanzar una moneda')
 
-st.write('Esta aplicación aún no es funcional. En construcción.')
+st.write('Esta aplicacion aun no es funcional. En construccion.')
